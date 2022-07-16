5397c913888d1673dde97d2361b8e10548a6ba4a
MARS <Sergei-6@bk.ru>
2022-07-16 21:34:29 +0300

    feat: config => combined brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+/*module.exports = */
+function check(str, bracketsConfig) {
+  // Формируем массив с парами ковычек
+  let arrStartEndBrackets = bracketsConfig.map((subArray) => { // Берем каждый подмасиив bracketsConfig и применяем reduce
+    return subArray.reduce((acc, value) => { // Возвращаем результат после каждой иттерации reduce
+      return acc + value; // Обьединяем элементы в подмассиве и возвращаем в acc
+    }, []);
+  });
+  // Формируем массив с парами ковычек
+
+  console.log(arrStartEndBrackets);
+
 }
+
+let bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
+console.log(check('()()()()][', bracketsConfig));
