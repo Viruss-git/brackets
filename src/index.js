c4681d144a64e42352040f5cfe0f1c9ac4736a26
MARS <Sergei-6@bk.ru>
2022-07-16 23:06:23 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-/*module.exports = */
-function check(str, bracketsConfig) {
+module.exports = function check(str, bracketsConfig) {
   // Формируем массив с парами ковычек
   let arrStartEndBrackets = bracketsConfig.map((subArray) => { // Берем каждый подмасиив bracketsConfig и применяем reduce
     return subArray.reduce((acc, value) => { // Возвращаем результат после каждой иттерации reduce
@@ -8,9 +7,22 @@ function check(str, bracketsConfig) {
   });
   // Формируем массив с парами ковычек
 
-  console.log(arrStartEndBrackets);
+  // Проверяем кол-во элементов в строке. Если кол-во нечетное, возвращаем false
+  // Если кол-во четное, продолжаем выполнение алгоритма
+  if (str.length % 2 == 0) {
+
+    for (let i = 0; i < arrStartEndBrackets.length; i++) {
+      console.log(str);
+      if (str.includes(arrStartEndBrackets[i])) {
+        str = str.replace(arrStartEndBrackets[i], '');
+        i = -1; // Сбрасываем i до -1 что бы выполнить скрипт когда в массиве останется одно значение [0]
+      }
+    }
+
+    return (str) ? false : true;
+
+  } else {
+    return false;
+  }
 
 }
-
-let bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];
-console.log(check('()()()()][', bracketsConfig));
